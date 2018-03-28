2242021bf59c73def01169950ecef7ca64668a16
sylence <sylence@sylence.org>
2018-03-28 16:06:55 -0600

    cleanup of old test utils

--- test/nokogiri.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-
-gem 'nokogiri'
-require 'nokogiri'
-require 'open-uri'
-doc = Nokogiri::HTML(open(ARGV[0]))
-puts doc.inspect
--- test/urlshortener.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env ruby
-
-require_relative '../lib/axial/irc_types/rb.rb'
-
-short_url = Axial::URIUtils.shorten(ARGV[0])
-puts short_url
