907f02560fd22f612e1d53a9a6b42d8422ce8c07
sylence <sylence@sylence.org>
2018-04-10 17:11:12 -0600

    test tool tweak

--- test/except.rb
+++ test/except.rb
@@ -8,11 +8,11 @@ require_relative '../lib/axial/models/rss_feed.rb'
 require_relative '../lib/axial/models/ban.rb'
 
 includes = {
-  :masks => { except: :id },
-  :seen => { except: [ :id, :user_id ] },
-  :things => { except: [ :id, :user_id ] },
-  :rss_feeds => { except: [ :id, :user_id ] },
-  :bans => { except: [ :id, :user_id ] }
+  masks: { except: :id },
+  seen: { except: [ :id, :user_id ] },
+  things: { except: [ :id, :user_id ] },
+  rss_feeds: { except: [ :id, :user_id ] },
+  bans: { except: [ :id, :user_id ] }
 }
 
 foo = JSON.parse(Axial::Models::User[name: 'fope'].to_json(include: includes, except: :id))
