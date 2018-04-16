d52be0db89908f31c2af6ab647df04e2542541ca
sylence <sylence@sylence.org>
2018-04-16 00:16:41 -0600

    fix stock quote terribads

--- lib/axial/api/iextrading/v1_0/stock/batch.rb
+++ lib/axial/api/iextrading/v1_0/stock/batch.rb
@@ -33,61 +33,10 @@ module Axial
 
               rest_endpoint       = URI.parse(rest_api)
               rest_endpoint.query = URI.encode_www_form(params)
-              response = RestClient::Request.execute(method: :get, url: rest_endpoint.to_s, headers: headers, verify_ssl: false)
-              json = JSON.parse(response)
+              json                = RestClient::Request.execute(method: :get, url: rest_endpoint.to_s, headers: headers, verify_ssl: false)
 
-              results             = {}
-
-              json.each do |symbol, data|
-                result = API::IEXTrading::V10::Stock::StockResult.new
-
-                if (data.key?('quote'))
-                  quote = data['quote']
-                  if (quote.key?('latestPrice'))
-                    result.latest_price = quote['latestPrice'].to_f
-                  end
-                  if (quote.key?('high'))
-                    result.high = quote['high'].to_f
-                  end
-                  if (quote.key?('low'))
-                    result.low = quote['low'].to_f
-                  end
-                  if (quote.key?('change'))
-                    result.change = quote['change'].to_f
-                  end
-                  if (quote.key?('open'))
-                    result.last_open = quote['open'].to_f
-                  end
-                  if (quote.key?('close'))
-                    result.last_close = quote['close'].to_f
-                  end
-                end
-
-                if (data.key?('news') && data['news'].is_a?(Array) && data['news'].any?)
-                  news = data['news'].first
-                  if (news.key?('headline'))
-                    result.news[:headline] = news['headline']
-                  end
-                  if (news.key?('datetime'))
-                    result.news[:date_time] = Time.parse(news['datetime'])
-                  end
-                end
-
-                if (data.key?('peers'))
-                  result.peers = data['peers']
-                end
-
-                if (data.key?('company'))
-                  company = data['company']
-                  if (company.key?('companyName'))
-                    result.company_name = company['companyName']
-                  end
-                end
-
-                result.symbol = symbol
-                results[symbol] = result
-              end
-              return results
+              results_array       = API::IEXTrading::V10::Stock::StockResult.array_from_json(json)
+              return results_array
             end
           end
         end
--- lib/axial/api/iextrading/v1_0/stock/stock_result.rb
+++ lib/axial/api/iextrading/v1_0/stock/stock_result.rb
@@ -4,20 +4,40 @@ module Axial
       module V10
         module Stock
           class StockResult
-            attr_accessor   :latest_price, :high, :low, :news, :peers, :change, :last_open,
-                            :last_close, :company_name, :symbol
+            attr_accessor   :latest_price, :high, :low, :change, :company_name, :symbol
 
             def initialize()
               @latest_price = 0.0
               @high         = 0.0
               @low          = 0.0
               @change       = 0.0
-              @last_open    = 0.0
-              @last_close   = 0.0
               @company_name = ''
-              @news         = {}
-              @peers        = []
               @symbol       = ''
+            end
+
+            def self.from_json_hash(symbol, json_hash)
+              result                = new
+              result.symbol         = symbol                                        || ''
+              result.latest_price   = json_hash.dig('quote', 'latestPrice')&.to_f   || 0.0
+              result.high           = json_hash.dig('quote', 'high')&.to_f          || 0.0
+              result.low            = json_hash.dig('quote', 'low')&.to_f           || 0.0
+              result.change         = json_hash.dig('quote', 'change')&.to_f        || 0.0
+              result.company_name   = json_hash.dig('company', 'companyName')       || symbol
+
+              return result
+            end
+
+            def self.array_from_json(json)
+              results       = []
+              json_hash     = JSON.parse(json)
+
+              json_hash.each do |symbol, data_hash|
+                result      = from_json_hash(symbol, data_hash)
+
+                results.push(result)
+              end
+
+              return results
             end
           end
         end
