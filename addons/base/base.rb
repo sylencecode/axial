493f65673014536d2c049b8c7d56bf3d7fa8fc29
sylence <sylence@sylence.org>
2018-04-17 16:35:10 -0600

    style guide updates, part of #102

--- addons/base/base.rb
+++ addons/base/base.rb
@@ -12,22 +12,24 @@ module Axial
 
         throttle                10
 
-        on_channel    'help',   :send_help
+        on_channel    'help',   :channel_send_help
         on_channel   'about',   :send_about
         on_channel  'reload',   :reload_addons
         on_channel_emote        :channel_emote
-        on_topic                :handle_topic_change
       end
 
-      def channel_emote(channel, nick, emote)
-        if (emote.split(/\s+/).collect { |words| words.downcase }.include?(@bot.real_nick))
-          timer.in_a_tiny_bit do
-            channel.emote('ducks')
-          end
+      def channel_emote(channel, _nick, emote)
+        words = emote.split(/\s+/)
+        if (words.select { |tmp_word| tmp_word.casecmp(myself.name).zero? }.empty?)
+          return
+        end
+
+        timer.in_a_tiny_bit do
+          channel.emote('ducks')
         end
       end
 
-      def send_about(channel, _nick, _command)
+      def send_about(channel, _nick, _command) # rubocop:disable Metrics/AbcSize
         addon_name_length = @bot.addons.collect { |tmp_addon| tmp_addon[:name].length }.max
         addon_version_length = @bot.addons.collect { |tmp_addon| tmp_addon[:version].to_s.length }.max
         channel.message("#{Colors.cyan}#{Constants::AXIAL_NAME}#{Colors.reset} version #{Constants::AXIAL_VERSION} by #{Constants::AXIAL_AUTHOR} (interpreter: ruby version #{RUBY_VERSION}p#{RUBY_PATCHLEVEL})")
@@ -41,66 +43,74 @@ module Axial
         end
       end
 
-      def change_topic(channel, nick, command)
-        if (command.args.empty?)
-          channel.message("#{nick.name}: current topic for #{channel.name} is: #{channel.topic}")
-        else
-          new_topic = command.args
-          if (channel.opped?)
-            channel.set_topic(new_topic)
-          end
-        end
-      end
-
-      def handle_topic_change(channel, nick, topic)
-        LOGGER.debug("new topic from #{nick.name}: #{topic}")
-      end
-
       def ctcp_ping_user(channel, nick, command)
         server.send_ctcp(nick, 'PING', Time.now.to_i.to_s)
       end
 
-      def send_help(channel, nick, command)
-        exclude_addons = [ 'axnet master', 'axnet slave', 'base' ]
-        channel.message("                    #{Colors.cyan}#{Constants::AXIAL_NAME}#{Colors.reset} version #{Constants::AXIAL_VERSION} by #{Constants::AXIAL_AUTHOR}")
-        channel.message(' ')
-        if (@bot.addons.any?)
-          addon_name_length = @bot.addons.collect { |tmp_addon| tmp_addon[:name].length }.max
-          all_string_commands = @bot.addons.collect { |tmp_addon| tmp_addon[:object].binds.collect { |tmp_bind| (tmp_bind[:type] == :channel && tmp_bind[:command].is_a?(String)) ? tmp_bind[:command] : nil } }.flatten
-          all_string_commands.delete_if { |command| command.nil? }
-          max_command_length = all_string_commands.collect{ |tmp_command| tmp_command.length }.max + 2
-          @bot.addons.each do |addon|
-            if (exclude_addons.include?(addon[:name].downcase))
-              next
-            end
+      def get_command_chunks(commands, max_command_length)
+        command_chunks = []
 
-            binds = addon[:object].binds.select { |bind| bind[:type] == :channel && bind[:command].is_a?(String) }
-            commands = binds.collect { |bind| bind[:command] }.sort_by { |command| command.gsub(/^\+/, '').gsub(/^-/, '') }.collect { |command| @bot.channel_command_character + command }
-            command_chunks = []
-            while (commands.count >= 6)
-              chunk = []
-              6.times do
-                tmp_command = commands.shift.ljust(max_command_length)
-                chunk.push(tmp_command)
-              end
-              command_chunks.push(chunk)
-            end
-
-            if (commands.any?)
-              command_chunks.push(commands.collect { |tmp_command| tmp_command.ljust(max_command_length) })
-            end
-
-            command_chunks.each_with_index do |chunk, i|
-              if (i.zero?)
-                channel.message("#{Colors.blue}#{addon[:name].rjust(addon_name_length)}#{Colors.reset} #{Colors.gray}|#{Colors.reset} #{chunk.join("#{Colors.gray} | #{Colors.reset}")}")
-              else
-                channel.message("#{' '.ljust(addon_name_length)} #{Colors.gray}|#{Colors.reset} #{chunk.join("#{Colors.gray} | #{Colors.reset}")}")
-              end
-            end
+        while (commands.count >= 6)
+          chunk = []
+          6.times do
+            tmp_command = commands.shift.ljust(max_command_length)
+            chunk.push(tmp_command)
           end
-        else
-          channel.message('no addons loaded.')
+          command_chunks.push(chunk)
         end
+
+        if (commands.any?)
+          command_chunks.push(commands.collect { |tmp_command| tmp_command.ljust(max_command_length) })
+        end
+
+        return command_chunks
+      end
+
+      def get_names_with_binds() # rubocop:disable Naming/AccessorMethodName
+        exclude_addons = [ 'axnet assistant', 'axnet master', 'axnet slave', 'base' ]
+        selected_addons = @bot.addons.reject { |tmp_addon| exclude_addons.include?(tmp_addon[:name].downcase) }
+        names_with_binds = selected_addons.collect { |tmp_addon| { name: tmp_addon[:name], binds: tmp_addon[:object].binds } }
+        names_with_binds.each do |name_with_binds|
+          name_with_binds[:binds].delete_if { |tmp_bind| tmp_bind[:type] != :channel || !tmp_bind[:command].is_a?(String) }
+          name_with_binds[:binds].collect! { |tmp_bind| @bot.channel_command_character + tmp_bind[:command] }
+          name_with_binds[:binds].sort_by! { |tmp_bind| tmp_bind.gsub(/^\+/, '').gsub(/^-/, '') }
+        end
+
+        names_with_binds.delete_if { |tmp_bind| tmp_bind[:binds].empty? }
+        return names_with_binds
+      end
+
+      def print_channel_commands(channel, names_with_binds) # rubocop:disable Metrics/AbcSize
+        if (names_with_binds.empty?)
+          return
+        end
+
+        addon_name_length = names_with_binds.collect { |tmp_bind| tmp_bind[:name].length }.max + 2
+        max_command_length = names_with_binds.collect { |tmp_bind| tmp_bind[:binds].collect(&:length).max }.max + 2
+
+        names_with_binds.each do |name_with_binds|
+          remaining_chunks = get_command_chunks(name_with_binds[:binds], max_command_length)
+
+          first_chunk = remaining_chunks.shift
+          channel.message("#{Colors.blue}#{name_with_binds[:name].rjust(addon_name_length)}#{Colors.reset} #{Colors.gray}|#{Colors.reset} #{first_chunk.join("#{Colors.gray} | #{Colors.reset}")}")
+          remaining_chunks.each do |tmp_chunk|
+            channel.message("#{' '.ljust(addon_name_length)} #{Colors.gray}|#{Colors.reset} #{tmp_chunk.join("#{Colors.gray} | #{Colors.reset}")}")
+          end
+        end
+      end
+
+      def channel_send_help(channel, _nick, _command)
+        channel.message("                    #{Colors.cyan}#{Constants::AXIAL_NAME}#{Colors.reset} version #{Constants::AXIAL_VERSION} by #{Constants::AXIAL_AUTHOR} (ruby version #{RUBY_VERSION}p#{RUBY_PATCHLEVEL})")
+        channel.message(' ')
+
+        if (@bot.addons.empty?)
+          channel.message('no addons loaded.')
+          return
+        end
+
+        names_with_binds = get_names_with_binds
+
+        print_channel_commands(channel, names_with_binds)
       rescue Exception => ex
         channel.message("#{self.class} error: #{ex.class}: #{ex.message}")
         LOGGER.error("#{self.class} error: #{ex.class}: #{ex.message}")
@@ -109,7 +119,7 @@ module Axial
         end
       end
 
-      def reload_addons(channel, nick, command)
+      def reload_addons(channel, nick, _command) # rubocop:disable Metrics/AbcSize
         user = user_list.get_from_nick_object(nick)
         if (user.nil? || !user.role.director?)
           return
@@ -117,17 +127,18 @@ module Axial
 
         if (@bot.addons.empty?)
           channel.message('no addons loaded.')
-        else
-          LOGGER.info("#{user.pretty_name} reloaded addons.")
-          addon_list = @bot.addons.reject { |addon| addon[:name] == 'base' }
-          addon_names = addon_list.collect { |addon| addon[:name] }
-          channel.message("unloading addons: #{addon_names.join(', ')}")
-          @bot.git_pull
-          @bot.reload_addons
-          addon_list = @bot.addons.reject { |addon| addon[:name] == 'base' }
-          addon_names = addon_list.collect { |addon| addon[:name] }
-          channel.message("loaded addons: #{addon_names.join(', ')}")
+          return
         end
+
+        LOGGER.info("#{user.pretty_name} reloaded addons.")
+        addon_list = @bot.addons.reject { |addon| addon[:name] == 'base' }
+        addon_names = addon_list.collect { |addon| addon[:name] }
+        channel.message("unloading addons: #{addon_names.join(', ')}")
+        @bot.git_pull
+        @bot.reload_addons
+        addon_list = @bot.addons.reject { |addon| addon[:name] == 'base' }
+        addon_names = addon_list.collect { |addon| addon[:name] }
+        channel.message("loaded addons: #{addon_names.join(', ')}")
       rescue Exception => ex
         channel.message("addon reload error: #{ex.class}: #{ex.message}")
         LOGGER.error("addon reload error: #{ex.class}: #{ex.message}")
